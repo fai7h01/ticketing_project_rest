df1b02dab122fc253cfbbf780202161ca3dd702b
fai7h01 <l.varsimashvil10@gmail.com>
2024-07-06 17:03:16 +0400

    UserController - message refactor

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -22,30 +22,30 @@ public class UserController {
     @GetMapping
     public ResponseEntity<ResponseWrapper> getUsers() {
         List<UserDTO> users = userService.listAllUsers();
-        return ResponseEntity.ok(new ResponseWrapper("User list retrieved successfully",users,HttpStatus.OK));
+        return ResponseEntity.ok(new ResponseWrapper("User list successfully retrieved",users,HttpStatus.OK));
     }
 
     @GetMapping("/{username}")
     public ResponseEntity<ResponseWrapper> getUserByUsername(@PathVariable("username") String username) {
         UserDTO userDTO = userService.findByUserName(username);
-        return ResponseEntity.ok(new ResponseWrapper("User is retrieved successfully", userDTO, HttpStatus.OK));
+        return ResponseEntity.ok(new ResponseWrapper("User is successfully retrieved", userDTO, HttpStatus.OK));
     }
 
     @PostMapping
     public ResponseEntity<ResponseWrapper> createUser(@RequestBody UserDTO user) {
         userService.save(user);
-        return ResponseEntity.status(HttpStatus.CREATED).body(new ResponseWrapper("User is created successfully",HttpStatus.CREATED));
+        return ResponseEntity.status(HttpStatus.CREATED).body(new ResponseWrapper("User is successfully created",HttpStatus.CREATED));
     }
 
     @PutMapping
     public ResponseEntity<ResponseWrapper> updateUser(@RequestBody UserDTO user) {
         userService.update(user);
-        return ResponseEntity.ok(new ResponseWrapper("User is updated successfully",HttpStatus.OK));
+        return ResponseEntity.ok(new ResponseWrapper("User is successfully updated",HttpStatus.OK));
     }
 
     @DeleteMapping("/{username}")
     public ResponseEntity<ResponseWrapper> deleteUser(@PathVariable("username") String username) {
         userService.delete(username);
-        return ResponseEntity.ok(new ResponseWrapper("User is deleted successfully",HttpStatus.OK));
+        return ResponseEntity.ok(new ResponseWrapper("User is successfully deleted",HttpStatus.OK));
     }
 }
