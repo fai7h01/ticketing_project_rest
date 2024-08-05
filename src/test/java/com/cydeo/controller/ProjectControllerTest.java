23397ea58e56ab59ec9207b22eed09f90aea71cf
fai7h01 <l.varsimashvil10@gmail.com>
2024-08-05 22:12:43 +0400

    integration testing

--- src/test/java/com/cydeo/controller/ProjectControllerTest.java
+++ src/test/java/com/cydeo/controller/ProjectControllerTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
@@ -25,11 +26,15 @@ class ProjectControllerTest {
     @Autowired
     private MockMvc mvc;
 
+    static String token;
+
     static UserDTO manager;
     static ProjectDTO project;
 
     @BeforeAll
     static void setUp(){
+
+        token = "Bearer " +  "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICI3cWVHRTBadGJDRjlmdW8wZGxPcV81SnFxT1RBcGVleEtXZEptQXgtdjFBIn0.eyJleHAiOjE3MjI4ODE2OTEsImlhdCI6MTcyMjg4MTM5MSwianRpIjoiMzMwOWRlM2YtYjJhZi00MmNkLTgyMjQtMmJlMDRhYzE3NDlkIiwiaXNzIjoiaHR0cDovL2xvY2FsaG9zdDo4MDgwL2F1dGgvcmVhbG1zL2N5ZGVvLWRldiIsImF1ZCI6ImFjY291bnQiLCJzdWIiOiJiMjAxZTI3Zi0yNmVhLTRmMDAtOGZiMi0yY2FjM2ZjYWVjZDQiLCJ0eXAiOiJCZWFyZXIiLCJhenAiOiJ0aWNrZXRpbmctYXBwIiwic2Vzc2lvbl9zdGF0ZSI6ImU5MDZkYmIzLTFkNmItNDQ5NS04NWVhLTllNmFhNmU5MmE2ZCIsImFjciI6IjEiLCJhbGxvd2VkLW9yaWdpbnMiOlsiaHR0cDovL2xvY2FsaG9zdDo4MDgxIl0sInJlYWxtX2FjY2VzcyI6eyJyb2xlcyI6WyJvZmZsaW5lX2FjY2VzcyIsImRlZmF1bHQtcm9sZXMtY3lkZW8tZGV2IiwidW1hX2F1dGhvcml6YXRpb24iXX0sInJlc291cmNlX2FjY2VzcyI6eyJ0aWNrZXRpbmctYXBwIjp7InJvbGVzIjpbIk1hbmFnZXIiXX0sImFjY291bnQiOnsicm9sZXMiOlsibWFuYWdlLWFjY291bnQiLCJtYW5hZ2UtYWNjb3VudC1saW5rcyIsInZpZXctcHJvZmlsZSJdfX0sInNjb3BlIjoib3BlbmlkIHByb2ZpbGUgZW1haWwiLCJzaWQiOiJlOTA2ZGJiMy0xZDZiLTQ0OTUtODVlYS05ZTZhYTZlOTJhNmQiLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwicHJlZmVycmVkX3VzZXJuYW1lIjoibHVrYSJ9.g93WDYwxplMM5JicvkVnn40UwW20rvDJzKmlpZ5bIaanJh9ednjm8XJpeaULbppXVbPjdLxMHFqQP9_R5IvajX4VKsZ498i0H0Hzw5LkqtdvvKc3TaRG_iC-iG9-YxK029xuoP4qCkayjcENxyNDa9cqVUQbPdCAdIhKlKRR-ajhaKMkpaoGzNLQIVT6PT7Pqv6cTzVYZ5IcIj87lpGnuL5qonZxahwiPs64Blgn70bnvRdDa0wXfyoMJU0gCe-jeieNTuOEG9CrWNiLYs9iVv6NQb9iBJDRKY31wE9bQSGEiX7z4qbqZJPJZOOn_Tago-fxhZlW1cET4zlTmpgndA";
 
         manager = new UserDTO(
                 2L,
@@ -58,6 +63,14 @@ class ProjectControllerTest {
     void givenNoToken_getProjects() throws Exception {
         mvc.perform(MockMvcRequestBuilders.get("/api/v1/project"))
                 .andExpect(status().is4xxClientError());
+    }
+
+    @Test
+    void givenToken_getProjects() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/api/v1/project")
+                .header("Authorization", token)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
     }
 
 }
